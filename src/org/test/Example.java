73f57ad003087332b98d74347ddf0cfc456f8115
liviya12 <liviya1232@gmail.com>
2018-09-25 11:57:18 +0530

    new class

---
+++ src/org/test/Example.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("first base class");
+		System.out.println("second base class");
+	}
+	
+
+}
